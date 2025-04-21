792e6f551596831d099e9d110afaef54d7627605
BreakableEggshell <triciagubatonmc64@gmail.com>
2025-04-21 17:43:00 +0800

    Shows details of anime

--- script/animeDetails.js
+++ script/animeDetails.js
@@ -6,23 +6,38 @@ const animeInfoFetch = async () => {
       `https://api.jikan.moe/v4/anime/${anime_MalID}`
     ).then((item) => item.json());
     data = response;
-    console.log(data);
+    // console.log(data); Debugging
   } catch (error) {
     console.error(
       "selectedAnimeItem sessionStorage is either empty or failed to fetch"
     );
     return;
   }
-  return data;
+  return data.data;
 };
 
 async function animeInfo() {
   const anime = await animeInfoFetch();
+  // console.log(anime); Debugging
 
   //1st Section
   const thumbnail = document.getElementById("anime-img");
+  thumbnail.src = anime.images.webp.large_image_url;
+
   const title = document.getElementById("title-text");
+  title.textContent = anime.titles[0].title;
+
   const studioList = document.getElementById("studio-text");
+  anime.studios.forEach((studio) => {
+    const studioItem = document.createElement("p");
+    studioItem.textContent = "• " + studio.name;
+    studioList.appendChild(studioItem);
+  });
+
+  const synopsis = document.getElementById("synopsis");
+  synopsis.textContent = anime.synopsis;
+
+  //2nd Section
 }
 
-document.addEventListener("DOMContentLoaded", animeInfo()); //Add function to load
+document.addEventListener("DOMContentLoaded", animeInfo());
